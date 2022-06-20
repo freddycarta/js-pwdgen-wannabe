666e5057051ef2ba00874f554a12a5bb967f49af
freddycarta <cartafederico92@gmail.com>
2022-06-20 15:57:05 +0200

    body

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 console.log ('js ok');
+
+const Name = prompt('come ti chiami?', 'will') 
+console.log(Name);
+
+const surname = prompt("qual'è il tuo cognome" , 'smith') 
+console.log(surname);
+
+const color = prompt("qual'è il tuo colore preferito", 'verde') 
+console.log(color);
+
+const hello = document .getElementById('hello')
+
+hello.innerText = 'name' + 'surname' + 'color' + '21'
