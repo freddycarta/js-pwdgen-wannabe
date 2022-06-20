a456e2fc9610ddde9384d35d2855d58d32314f01
freddycarta <cartafederico92@gmail.com>
2022-06-20 16:35:11 +0200

    last commit

--- script.js
+++ script.js
@@ -3,12 +3,15 @@ console.log ('js ok');
 const Name = prompt('come ti chiami?', 'will') 
 console.log(Name);
 
-const surname = prompt("qual'è il tuo cognome" , 'smith') 
+const surname = prompt("qual'è il tuo cognome?" , 'smith') 
 console.log(surname);
 
-const color = prompt("qual'è il tuo colore preferito", 'verde') 
+const color = prompt("qual'è il tuo colore preferito?", 'verde') 
 console.log(color);
 
 const hello = document .getElementById('hello')
 
-hello.innerText = 'name' + 'surname' + 'color' + '21'
+
+document.getElementById('hello').innerText = Name + surname + color + '21'
+
+// document.getElementById('TUOID').innerText = `${Name}${surname}${color}21`
